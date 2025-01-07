2aea01d5b41926731613e8de74eedf5506159c7e
Max <ninjamlgpro75@gmail.com>
2025-01-07 10:34:51 -0500

    Added information about variables

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,12 @@
 # Python 2 is legacy, python 3 is the newest version (might eventually be a python4)
+print("Hello World!")
+# These are comments, you know what they are
+# we can draw cool shapes with python, i don't need to write them though cause i get it
+
+
+    # Variables:
+#Variables are kind of terrible in python because they don't have any types
+name = "Day"
+x = "17"
+print("I " + " my life")
+print("Woops, that was a bad introduction, my name is " + name + " and i'm " + x + " years old")
